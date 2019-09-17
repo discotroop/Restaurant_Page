8910e5a3b522e697bfdf4cbc026995be9e4f3579
John Mark Goodson <john.mark.hat@gmail.com>
2019-09-17 19:06:10 -0400

    index.js complete and properly linked to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import {initial} from "./initial.js";
 initial();
 
-console.log("test webpack")
+import {about} from "./about.js";
+import {menu} from "./menu.js";
+import {contact} from "./contact.js";
+
+let runAbout = document.querySelector("#about");
+runAbout.addEventListener("click", function() {about()});
+
+let runMenu = document.querySelector("#menu");
+runMenu.addEventListener("click", function() {menu()});
+
+let runContact = document.querySelector("#contact");
+runContact.addEventListener("click", function() {
+    contact()
+});
+
+about();
